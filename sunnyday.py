fe0fcf4fe62583a84ef8bf341c6bfc79133c741e
Yohannes Getu <nede.evoli@gmail.com>
2021-08-28 15:47:47 +0300

    added documentation

--- sunnyday.py
+++ sunnyday.py
@@ -4,6 +4,27 @@ import requests
 
 
 class Weather:
+    """Creates a weather object getting an apikey as input
+    and either a city name or lat and lon coordinates.
+
+    Package use example:
+
+    # Create a weather object using a city name:
+    # The api key below is not guaranteed to work.
+    # Get your own apikey from https://openweathermap.org
+    # And wait a couple of hours for the apikey to be activated
+
+    >>> weather1 = Weather(apikey = "26631f0f41b95fb9f5ac0df9a8f43c92", city="Addis Ababa")
+
+    # Using latitude and longitude coordinates
+    >>> weather2 = Weather(apikey = "26631f0f41b95fb9f5ac0df9a8f43c92", lat=9.025, lon=38.7469)
+
+    # Get complete weathe data for the nexy we hours:
+    >>> weather1.next_12h()
+
+    # Simplified data for the next 12 hours:
+    >>> weather1.next_12h_simplified()
+    """
     def __init__(self, apikey, city=None, lat=None, lon=None, units="metric"):
         self.apikey = apikey
         self.city = city
@@ -26,9 +47,14 @@ class Weather:
             raise ValueError(self.data["message"])
 
     def next_12h(self):
+        """ returns 3-hour data for the next 12 hours as dict.
+        """
         return self.data['list'][:4]
 
     def next_12h_simplified(self):
+        """returns date, temperature, and sky condition every 3 hours
+        for the next 12 hours as a tuple of tuples
+        """
         simple_data = []
         for dicty in self.data['list'][:4]:
             simple_data.append((dicty['dt_txt'],
